ebbd977ff994033577ce9399696fd9ac12ea50d3
iMac <Triforce1@Nirmals-iMac.local>
2014-05-30 14:19:47 +0530

    Initial Commit

---
+++ FailedBankCD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FailedBankCD
+//
+//  Created by iMac on 30/05/14.
+//  Copyright (c) 2014 Triforce-1-Imac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
